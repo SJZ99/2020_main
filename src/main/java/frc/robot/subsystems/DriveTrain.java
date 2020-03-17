98f32671f92cc7f6c279ad9e37c430b33c3c7491
SJZ99 <9987jdvrb@gmail.com>
2020-03-18 07:23:23 +0800

    modifing the problem

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
@@ -15,6 +16,7 @@ import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
@@ -26,7 +28,8 @@ public class DriveTrain extends SubsystemBase {
   WPI_TalonFX leftmotor         = new WPI_TalonFX(Constants.MotorConstants.kleftmotor);
   WPI_TalonFX leftmotorSlave   = new WPI_TalonFX(Constants.MotorConstants.kleftmotorS);
 
-  DifferentialDrive drive = new DifferentialDrive(leftmotor, rightmotor);
+  private double m_quickStopAccumulator = 0,leftout=0,rightout=0;
+
   public DriveTrain() {
     setmotor();
 
@@ -99,6 +102,92 @@ public class DriveTrain extends SubsystemBase {
    */
   public double getrightPosition(){
     return rightmotor.getSelectedSensorPosition(0) * Constants.MotorConstants.distantsPerPulse;
+  }
+
+   /**
+   * Curvature drive method for differential drive platform.
+   *
+   * <p>The rotation argument controls the curvature of the robot's path rather than its rate of
+   * heading change. This makes the robot more controllable at high speeds. Also handles the
+   * robot's quick turn functionality - "quick turn" overrides constant-curvature turning for
+   * turn-in-place maneuvers.
+   *
+   * @param xSpeed      The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+   * @param zRotation   The robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is
+   *                    positive.
+   * @param isQuickTurn If set, overrides constant-curvature turning for
+   *                    turn-in-place maneuvers.
+   */
+  @SuppressWarnings({"ParameterName", "PMD.CyclomaticgitComplexity"})
+  public void curvatureDrive(double xSpeed, double zRotation, boolean isQuickTurn) {
+
+    xSpeed = MathUtil.clamp(xSpeed, -1.0, 1.0);
+    if(Math.abs(zRotation)<0.05){
+      zRotation =0;
+    }
+    zRotation = MathUtil.clamp(zRotation, -1.0, 1.0);
+
+    double angularPower;
+    boolean overPower;
+    double m_quickStopAlpha =0.1;
+    if (isQuickTurn) {
+      if (Math.abs(xSpeed) < 0.1) {
+        m_quickStopAccumulator = (1 - 0.1) * m_quickStopAccumulator
+            + m_quickStopAlpha * MathUtil.clamp(zRotation, -1.0, 1.0) * 2;
+      }
+      
+      overPower = true;
+      angularPower = zRotation;
+    } else {
+      overPower = false;
+      angularPower = Math.abs(xSpeed) * zRotation - m_quickStopAccumulator;
+
+      if (m_quickStopAccumulator > 1) {
+        m_quickStopAccumulator -= 1;
+      } else if (m_quickStopAccumulator < -1) {
+        m_quickStopAccumulator += 1;
+      } else {
+        m_quickStopAccumulator = 0.0;
+      }
+    }
+
+    double leftMotorOutput = xSpeed + angularPower;
+    double rightMotorOutput = xSpeed - angularPower;
+
+    // If rotation is overpowered, reduce both outputs to within acceptable range
+    if (overPower) {
+      if (leftMotorOutput > 1.0) {
+        rightMotorOutput -= leftMotorOutput - 1.0;
+        leftMotorOutput = 1.0;
+      } else if (rightMotorOutput > 1.0) {
+        leftMotorOutput -= rightMotorOutput - 1.0;
+        rightMotorOutput = 1.0;
+      } else if (leftMotorOutput < -1.0) {
+        rightMotorOutput -= leftMotorOutput + 1.0;
+        leftMotorOutput = -1.0;
+      } else if (rightMotorOutput < -1.0) {
+        leftMotorOutput -= rightMotorOutput + 1.0;
+        rightMotorOutput = -1.0;
+      }
+    }
+
+    // Normalize the wheel speeds
+    double maxMagnitude = Math.max(Math.abs(leftMotorOutput), Math.abs(rightMotorOutput));
+    if (maxMagnitude > 1.0) {
+      leftMotorOutput /= maxMagnitude;
+      rightMotorOutput /= maxMagnitude;
+    }
+
+    leftout=  leftMotorOutput;
+    rightout = rightMotorOutput;
+    
+  }
+  public void curveDrive(double xSpeed, double zRotation, boolean isQuickTurn){
+    
+    curvatureDrive(xSpeed, zRotation, isQuickTurn);
+
+    rightmotor.set(ControlMode.PercentOutput, rightout);
+    leftmotor.set(ControlMode.PercentOutput, leftout);
   }
 
   @Override
